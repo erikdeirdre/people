bd66274df2efe640248dd93ea5e4a0d2b40f265a
syncpeterwhite <peter.white@synccomputing.com>
2022-03-20 19:11:36 -0400

    change to getenv

--- app/settings.py
+++ app/settings.py
@@ -1,21 +1,21 @@
-from os import environ
+from os import getenv
 
 APP_NAME = 'people'
 # Default to info
-LOG_LEVEL = int(environ.get("LOG_LEVEL", "20"))
+LOG_LEVEL = int(getenv("LOG_LEVEL", "20"))
 
-SQLALCHEMY_URI = environ.get("SQLALCHEMY_URI")
+SQLALCHEMY_URI = getenv("SQLALCHEMY_URI")
 SQLALCHEMY_DATABASE_URI = SQLALCHEMY_URI
 
-SECRET_KEY = environ.get('SECRET_KEY', 'my_secret')
-SECRET_TIMEOUT = int(environ.get('SECRET_TIMEOUT', '900'))
-DEBUG = environ.get('DEBUG', False)
-TESTING = environ.get('TESTING', False)
+SECRET_KEY = getenv('SECRET_KEY', 'my_secret')
+SECRET_TIMEOUT = int(getenv('SECRET_TIMEOUT', '900'))
+DEBUG = getenv('DEBUG', False)
+TESTING = getenv('TESTING', False)
 BCRYPT_LOG_ROUNDS = 13
 
 # Flask-security settings
 SECURITY_PASSWORD_HASH = 'pbkdf2_sha512'
-SECURITY_PASSWORD_SALT = environ.get('PASSWORD_SALT') or 'mySalt'
+SECURITY_PASSWORD_SALT = getenv('PASSWORD_SALT') or 'mySalt'
 SECURITY_TOKEN_AUTHENTICATION_KEY = 'auth_token'
 SECURITY_TOKEN_AUTHENTICATION_HEADER = 'Authentication-Token'
 SECURITY_TOKEN_MAX_AGE = 1800
@@ -29,17 +29,17 @@ SECURITY_LOGIN_URL = '/login'
 WTF_CSRF_ENABLED = False
 
 # Email
-MAIL_SERVER = environ.get('MAIL_SERVER', 'smtp.sendgrid.net')
-MAIL_PORT = int(environ.get('MAIL_PORT', '587'))
-MAIL_USE_TLS = environ.get('MAIL_USE_TLS', True)
-MAIL_USE_SSL = environ.get('MAIL_USE_SSL', False)
-MAIL_USERNAME = environ.get('MAIL_USERNAME')
-MAIL_PASSWORD = environ.get('MAIL_PASSWORD')
-MAIL_DEFAULT_SENDER = environ.get('MAIL_DEFAULT_SENDER')
+MAIL_SERVER = getenv('MAIL_SERVER', 'smtp.sendgrid.net')
+MAIL_PORT = int(getenv('MAIL_PORT', '587'))
+MAIL_USE_TLS = getenv('MAIL_USE_TLS', True)
+MAIL_USE_SSL = getenv('MAIL_USE_SSL', False)
+MAIL_USERNAME = getenv('MAIL_USERNAME')
+MAIL_PASSWORD = getenv('MAIL_PASSWORD')
+MAIL_DEFAULT_SENDER = getenv('MAIL_DEFAULT_SENDER')
 
 # Admin account
-ADMIN_PASSWORD = environ.get('ADMIN_PASSWORD', 'password123#')
-ADMIN_EMAIL = environ.get('ADMIN_EMAIL', 'admin@example.com')
+ADMIN_PASSWORD = getenv('ADMIN_PASSWORD', 'password123#')
+ADMIN_EMAIL = getenv('ADMIN_EMAIL', 'admin@example.com')
 EMAIL_SUBJECT_PREFIX = f"[{APP_NAME}]"
 EMAIL_SENDER = f"{APP_NAME} Admin <{MAIL_USERNAME}>"
 
@@ -48,4 +48,4 @@ SOCIAL_GOOGLE = {
     'consumer_secret': "password"
 }
 
-GRAPHIQL = environ.get('GRAPHIQL', False)
+GRAPHIQL = getenv('GRAPHIQL', False)
